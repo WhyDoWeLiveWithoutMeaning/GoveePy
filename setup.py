6a25887d981fce549d63938cd0a874bd4e773b02
Eric Beaulne <eric.beaulne@ontariotechu.net>
2023-12-08 23:45:16 -0500

    Added Readme

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as f:
+  long_description = f.read()
+
 setup(
   name='GoveePy',
   version='0.3',
   description='A Govee API Wrapper for Python',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   url='https://github.com/WhyDoWeLiveWithoutMeaning/GoveePy',
   author="Meaning",
   license="MIT",
